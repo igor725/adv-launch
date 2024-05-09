9376e9434d813639dd6ffadf7f3c9dcca443ad5b
igor725 <gvaldovigor@gmail.com>
2024-05-09 15:42:42 +0300

    Add ffplay and 7z

--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const cfgfile = path.join(__dirname, '/config.json');
+
 const config = {
   default: {
     update_channel: 'release',
@@ -10,10 +12,22 @@ const config = {
 };
 
 try {
-  const data = fs.readFileSync(path.join(__dirname, '/config.json'), { encoding: 'utf-8' });
+  const data = fs.readFileSync(cfgfile, { encoding: 'utf-8' });
   config.json = JSON.parse(data);
 } catch (e) {
   console.error('Failed to load the default config: ', e.toString());
+  const data = JSON.stringify(config.data);
+  config.json = JSON.parse(data);
+  fs.writeFileSync(cfgfile, data);
 }
+
+config.getValue = (name) => {
+  if (!config.default[name]) {
+    console.error('Attempt to get ')
+    return null;
+  }
+};
+
+config.getVolume = () => { };
 
 module.exports = config;
